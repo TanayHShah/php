2484314ed3e6095a23730750a0a637dc03327cb4
TanayHShah <tanayhshah@gmail.com>
2020-01-20 17:58:07 +0530

    Day 2 Practice

---
+++ 21-01-2020/array.php
@@ -0,0 +1,17 @@
+<?php 
+$day = array('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday');
+echo 'The whole array is : ';
+print_r($day);
+echo '<br>';
+echo '<br>Particular element of array displayed is(first element) : '.$day[0].'<br>';
+$day = array('Monday'=>100,'Tuesday'=>200,'Wednesday'=>300,'Thursday'=>400,'Friday'=>500,'Saturday'=>600,'Sunday'=>700);
+echo '<br>Array after assigning different values to elements';
+print_r($day);
+echo '<br>';
+$day = array('Weekdays'=>array('Monday','Tuesday','Wednesday','Thursday','Friday'), 'Weekends'=>array('Saturday','Sunday'));
+echo '<br>Array inserted inside an array<br>';
+print_r($day);
+echo '<br>';
+echo '<br>Displaying Single Element from MultiDimensional Array : '.$day['Weekdays'][0];
+
+?>
