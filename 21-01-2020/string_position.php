844bad9720e96b623d66a0c917e2fbb7bb01a523
TanayHShah <tanayhshah@gmail.com>
2020-01-21 11:47:41 +0530

    Using strpos Function

--- 21-01-2020/string_position.php
+++ 21-01-2020/string_position.php
@@ -1,24 +1,23 @@
 <style>
-label{
-    display: inline-block;
-    width:100px;
-}
-
+    label {
+        display: inline-block;
+        width: 100px;
+    }
 </style>
 <form action="string_position.php" method="POST">
-<label>Enter a String : </label><textarea rows="3" cols="100" name="input" required></textarea><br>
-<br><label>Enter sub-string you want to find : </label><textarea rows="3" cols="100" name="sub_input" required></textarea><br>
-<br><input type="submit">
+    <label>Enter a String : </label><textarea rows="3" cols="100" name="input" required></textarea><br>
+    <br><label>Enter sub-string you want to find : </label><textarea rows="3" cols="100" name="sub_input" required></textarea><br>
+    <br><input type="submit">
 </form>
 <?php
-$input_string=$_POST['input'];
-$sub_input_string=$_POST['sub_input'];
-$length=strlen($sub_input_string);
-$offset=0;
-echo '<br>Input String is : '.$input_string.'<br>';
-echo '<br>Sub-String to find is : '.$sub_input_string.'<br>';
-while($string_position=strpos($input_string,$sub_input_string,$offset)){
-    echo '<br>'.$sub_input_string.' found at postion '.$string_position.'<br>';
-    $offset=$string_position + $length;
+$input_string = $_POST['input'];
+$sub_input_string = $_POST['sub_input'];
+$length = strlen($sub_input_string);
+$offset = 0;
+echo '<br>Input String is : ' . $input_string . '<br>';
+echo '<br>Sub-String to find is : ' . $sub_input_string . '<br>';
+while ($string_position = strpos($input_string, $sub_input_string, $offset)) {
+    echo '<br>' . $sub_input_string . ' found at postion ' . $string_position . '<br>';
+    $offset = $string_position + $length;
 }
 ?>
