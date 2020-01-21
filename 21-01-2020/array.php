16aeafd1b2d67399dda1cc5573508a854bcef619
TanayHShah <tanayhshah@gmail.com>
2020-01-21 09:57:00 +0530

    Using array

--- 21-01-2020/array.php
+++ 21-01-2020/array.php
@@ -1,17 +1,27 @@
-<?php 
-$day = array('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday');
+<?php
+$day = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
 echo 'The whole array is : ';
 print_r($day);
 echo '<br>';
-echo '<br>Particular element of array displayed is(first element) : '.$day[0].'<br>';
-$day = array('Monday'=>100,'Tuesday'=>200,'Wednesday'=>300,'Thursday'=>400,'Friday'=>500,'Saturday'=>600,'Sunday'=>700);
+echo '<br>Particular element of array displayed is(first element) : ' . $day[0] . '<br>';
+$day = array('Monday' => 100, 'Tuesday' => 200, 'Wednesday' => 300, 'Thursday' => 400, 'Friday' => 500, 'Saturday' => 600, 'Sunday' => 700);
 echo '<br>Array after assigning different values to elements';
 print_r($day);
 echo '<br>';
-$day = array('Weekdays'=>array('Monday','Tuesday','Wednesday','Thursday','Friday'), 'Weekends'=>array('Saturday','Sunday'));
+$day = array('Weekdays' => array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'), 'Weekends' => array('Saturday', 'Sunday'));
 echo '<br>Array inserted inside an array<br>';
 print_r($day);
 echo '<br>';
-echo '<br>Displaying Single Element from MultiDimensional Array : '.$day['Weekdays'][0];
-
+echo '<br>Displaying Single Element from MultiDimensional Array : ' . $day['Weekdays'][0] . '<br>';
+echo '<br>Displaying MultiDimentional array as list<br>';
+echo '<br>';
+foreach ($day as $element => $inner_array) {
+    echo '<strong>' . $element . '</strong>';
+    foreach ($inner_array as $item) {
+        echo '<br>';
+        echo $item;
+        echo '<br>';
+    }
+    echo '<br>';
+}
 ?>
