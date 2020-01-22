2a3699ea8d5962ca46f96fef9cc331fd5e3f17c2
TanayHShah <tanayhshah@gmail.com>
2020-01-22 17:02:11 +0530

    Practice Sheet

--- extra practice/increasing_pattern.php
+++ extra practice/increasing_pattern.php
@@ -1,8 +1,22 @@
-<?php
-for($i=0;$i<=8;$i++){
-    for($j=1;$j<=$i;$j++){
-        echo $j.'&nbsp&nbsp';
+<style>
+    label {
+        display: inline-block;
+        width: 350px;
     }
-    echo '<br>';
+</style>
+<form action="increasing_pattern.php" method="GET">
+    <label>Enter Number Of Rows: </label><input type="number" name="first_number" required min="0"><br>
+    <br><input type="submit"><br>
+
+</form>
+<?php
+if (isset($_GET['first_number'])) {
+    $row = $_GET['first_number'];
+    for ($i = 0; $i <= $row; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo $j . '&nbsp&nbsp';
+        }
+        echo '<br>';
+    }
 }
 ?>
